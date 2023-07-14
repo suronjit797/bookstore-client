af7e41bf32b8f548586e7f56515d283a528a090d
suronjit797 <suronjit797@gmail.com>
2023-07-15 01:25:27 +0600

    books search

--- src/pages/books/Books.tsx
+++ src/pages/books/Books.tsx
@@ -1,5 +1,6 @@
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
+import { useState, FormEvent } from "react";
 import Loading from "../../components/Loading/Loading";
 import BookCard from "../../components/bookCard/BookCard";
 import { IBook } from "../../interface/bookInterface";
@@ -8,13 +9,16 @@ import { useGetBooksQuery } from "../../redux/features/books/bookApi";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import { BsSearch } from "react-icons/bs";
 
 const Books = () => {
-  const { data, isLoading, error } = useGetBooksQuery("");
+  const [searchTerm, setSearchTerm] = useState("");
+  const [search, setSearch] = useState("");
+  const { data, isLoading, error } = useGetBooksQuery(
+    `${searchTerm ? "searchTerm=" + searchTerm : ""}`
+  );
 
   const books: IBook[] = data?.data as IBook[];
-
-  console.log(data);
 
   if (isLoading) {
     return <Loading />;
@@ -24,10 +28,31 @@ const Books = () => {
     console.log(error);
   }
 
+  const searchHandler = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    setSearchTerm(search);
+  };
+
   return (
     <Layout>
       <Container>
-        <Row xs={1} sm={2} md={3} lg={4} className="my-3">
+        <div className="d-flex justify-content-between mt-4">
+          <h4> BookLists </h4>
+          <form className="d-flex" onSubmit={searchHandler}>
+            <input
+              type="search"
+              name="search"
+              id="search"
+              className="form-control rounded-0"
+              onChange={(e) => setSearch(e.target.value)}
+            />
+            <button className="btn btn-primary rounded-0" type="submit">
+              <BsSearch />
+            </button>
+          </form>
+        </div>
+        <hr />
+        <Row xs={1} sm={2} md={3} lg={4} className="my-3 g-4">
           {books.length > 0 &&
             books.map((book) => (
               <Col key={book._id}>
